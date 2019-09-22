0b9986255af529b755f62f7aed09bdac06a56355
S01302060 <sas23@rice.edu>
2019-09-21 21:52:16 -0500

    werghrtfgftg

--- boilerplate/HR9_Popup/background.js
+++ boilerplate/HR9_Popup/background.js
@@ -7,13 +7,12 @@ function howLongOnSite (website)
     return function()
     {
         webInfo [website] = webInfo [website] + 5;
-        
         chrome.storage.local.set({'key': JSON.stringify(webInfo)}, function() {
             console.log('I promise it saved');
         });
 
-
        chrome.storage.local.get(['key'], function(result) {
+        console.log("The following line is the actual WebInfo Shit");
         console.log(JSON.parse(result.key));
       });
     }
@@ -40,19 +39,19 @@ chrome.tabs.onActivated.addListener (
 
 //control the popup
 window.onload = function () {
-    console.log("window has loaded");
+    console.log("popup has loaded");
     list = [];
     var dictionary = {}
     chrome.storage.local.get(['key'], function(result) {
-        dictionary = result.key;
-        console.log("data has been loaded");
+        dictionary = JSON.parse(result.key);
+        console.log("Have loaded the saved data onto the pop as follows");
+        console.log(dictionary);
     });
-    console.log(dictionary);
     
-    Object.keys(dictionary.forEach(function(key) {
+    /*Object.keys(dictionary.forEach(function(key) {
         list.push({y:dictionary[key],label:key});
         document.getElementById("timeInSeconds").textContent = dictionary[key]
     }));
-    
+    */
     
 }
