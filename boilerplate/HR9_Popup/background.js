102876e540ca5f4e045fcaf493cbb573f6a90c41
S01302060 <sas23@rice.edu>
2019-09-21 22:26:49 -0500

    botch

--- boilerplate/HR9_Popup/background.js
+++ boilerplate/HR9_Popup/background.js
@@ -39,37 +39,3 @@ chrome.tabs.onActivated.addListener (
 );
 
 //control the popup
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("popup has loaded");
-    list = [];
-    var dictionary = {}
-    //this code doesn't actually run FOR SOME FUCKING REASON
-    chrome.storage.local.get(['key'], function(result) {
-        dictionary = JSON.parse(result.key);
-        console.log("Have loaded the saved data onto the pop as follows");
-        console.log(dictionary);
-    });
-    
-    //for every website
-    Object.keys(dictionary).forEach(function(key) {
-        //create a text element that displays the website name
-        var para = document.createElement(key);
-        var node = document.createTextNode(key);
-        para.appendChild(node);
-
-        var element = document.getElementById("contents");
-        element.appendChild(para);
-        //create a text element that displays the amount of time on the site
-        var para2 = document.createElement(key);
-        var node2 = document.createTextNode(dictionary[key]);
-        para2.appendChild(node2);
-
-        element.appendChild(para2);
-    });
-    /*Object.keys(dictionary.forEach(function(key) {
-        list.push({y:dictionary[key],label:key});
-        document.getElementById("timeInSeconds").textContent = dictionary[key]
-    }));
-    */
-    
-});
--- boilerplate/HR9_Popup/popup.js
+++ boilerplate/HR9_Popup/popup.js
@@ -6,6 +6,32 @@ document.addEventListener('DOMContentLoaded', function() {
      window.open('https://reallazbaz.github.io/HR9-First-Timers-Track/');
   }, false);
   
+  console.log("popup has loaded");
+  list = [];
+  var dictionary = {}
+  //this code doesn't actually run FOR SOME FUCKING REASON
+  chrome.storage.local.get(['key'], function(result) {
+      dictionary = JSON.parse(result.key);
+      console.log("Have loaded the saved data onto the pop as follows");
+      console.log(dictionary);
+  });
+  
+  //for every website
+  Object.keys(dictionary).forEach(function(key) {
+      //create a text element that displays the website name
+      var para = document.createElement(key);
+      var node = document.createTextNode(key);
+      para.appendChild(node);
+
+      var element = document.getElementById("contents");
+      element.appendChild(para);
+      //create a text element that displays the amount of time on the site
+      var para2 = document.createElement(key);
+      var node2 = document.createTextNode(dictionary[key]);
+      para2.appendChild(node2);
+
+      element.appendChild(para2);
+  });
  
   
 });
